6db4e3e74af25befe32cfd04b0f0836900318d93
owolana ayomide <ayomideowolana1@gmail.com>
2022-02-10 15:24:27 +0100

    added router to form

--- src/components/form.js
+++ src/components/form.js
@@ -8,6 +8,7 @@ import Summary from "./summary";
 import Certification from "./cetification";
 import Picture from "./picture";
 import Languages from "./languages";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 export default function Form({
   setNames,
@@ -22,15 +23,88 @@ export default function Form({
 }) {
   return (
     <div className="col-lg-6" id="form">
-      <Picture updatePicture={updatePicture} />
-      <Names setNames={setNames} />
-      <Contact setContact={setContact} />
-      <Summary updateSummary={updateSummary} />
-      <Skills updateSkills={updateSkills} />
-      <Experiences updateExperiences={updateExperiences} />
-      <Education updateEducation={updateEducation} />
-      <Certification updateCertification={updateCertification} />
-      <Languages updateLanguages={updateLanguages} />
+      <div>
+        <Router>
+          <div>
+            <ul className="form-nav">
+              <li>
+                <Link to="/">Picture</Link>
+              </li>
+              <li>
+                <Link to="/names">Names</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contact</Link>
+              </li>
+              <li>
+                <Link to="/summary">Summary</Link>
+              </li>
+              <li>
+                <Link to="/skills">Skills</Link>
+              </li>
+              <li>
+                <Link to="/experiences">Experiences</Link>
+              </li>
+              <li>
+                <Link to="/education">Education</Link>
+              </li>
+              <li>
+                <Link to="/certification">Certification</Link>
+              </li>
+              <li>
+                <Link to="/languages">Languages</Link>
+              </li>
+            </ul>
+          </div>
+          <Routes>
+            <Route
+              path="/"
+              element={<Picture updatePicture={updatePicture} />}
+            />
+            <Route path="/names" element={<Names setNames={setNames} />} />
+            <Route
+              path="/contact"
+              element={<Contact setContact={setContact} />}
+            />
+            <Route
+              path="/summary"
+              element={<Summary updateSummary={updateSummary} />}
+            />
+            <Route
+              path="/skills"
+              element={<Skills updateSkills={updateSkills} />}
+            />
+            <Route
+              path="/experiences"
+              element={<Experiences updateExperiences={updateExperiences} />}
+            />
+            <Route
+              path="/education"
+              element={<Education updateEducation={updateEducation} />}
+            />
+            <Route
+              path="/certification"
+              element={
+                <Certification updateCertification={updateCertification} />
+              }
+            />
+            <Route
+              path="/languages"
+              element={<Languages updateLanguages={updateLanguages} />}
+            />
+          </Routes>
+        </Router>
+      </div>
+      {/* 
+      
+      
+      
+      
+      
+      
+      
+      
+      */}
     </div>
   );
 }
